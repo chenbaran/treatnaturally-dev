4f49914dd17cbf63849972e75a92ab826546fbc3
chenbaran <112110630+chenbaran@users.noreply.github.com>
2023-01-24 20:27:59 +0200

    11.24.2023 - 20:24 - Instaled ckeditor for rich text fields, replaced blog model textfield with richtexteditor

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from .validators import validate_file_size
+from ckeditor.fields import RichTextField
 
 class BlogPost(models.Model):
     title = models.CharField(max_length=255)
-    content = models.TextField(null=True, blank=True)
+    content = RichTextField(null=True, blank=True)
     last_update = models.DateTimeField(auto_now=True)
 
     def __str__(self) -> str:
