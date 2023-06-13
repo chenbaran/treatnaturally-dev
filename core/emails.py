174a183fade5242dedd263501f04cfd1796c3510
unknown <chen.baran@gmail.com>
2023-06-13 16:59:30 +0300

    13.06.2023 - 16:59 - added signal to send mail to admin on every new contact form entry

--- core/emails.py
+++ core/emails.py
@@ -15,3 +15,15 @@ def send_user_registration_mail(user):
     recipient_list = [user.email]
     html_message = f'<html><body style="font-family: Roboto">{message}</body></html>'
     send_mail(subject, message, from_email, recipient_list, html_message=html_message)
+
+def send_contact_form_entry_alert_mail(contactentry):
+    subject = f'New Contact Form Entry #{contactentry.id}'
+    message = f'<h3>New contact form entry:</h3>'
+    message += f'<h4>Name: {contactentry.name}</h4>'
+    message += f'<h4>Email: {contactentry.email}</h4>'
+    message += f'<h4>Subject: {contactentry.subject}</h4>'
+    message += f'<h4>Message: {contactentry.message}</h4>'
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_list = ['info@treatnaturally.co.uk']
+    html_message = f'<html><body style="font-family: Roboto">{message}</body></html>'
+    send_mail(subject, message, from_email, recipient_list, html_message=html_message)
--- core/signals/handlers.py
+++ core/signals/handlers.py
@@ -3,9 +3,10 @@ from django.conf import settings
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 from django.db import transaction
-from core.emails import send_user_registration_mail
+from core.emails import send_user_registration_mail, send_contact_form_entry_alert_mail
 from store.signals import order_created
 from django.db.models.signals import pre_save
+from core.models import ContactFormEntry
 
 @receiver(order_created)
 def on_order_created(sender, **kwargs):
@@ -17,3 +18,10 @@ def on_order_created(sender, **kwargs):
 def send_registration_success(sender, instance, **kwargs):
     if instance._state.adding:
         transaction.on_commit(lambda: send_user_registration_mail(instance))
+
+
+# send new contact form entry alert to admin
+@receiver(post_save, sender=ContactFormEntry)
+def send_order_confirmation(sender, instance, created, **kwargs):
+    if created:
+        transaction.on_commit(lambda: send_contact_form_entry_alert_mail(instance))
