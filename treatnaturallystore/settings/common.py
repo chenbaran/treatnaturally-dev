b51f4fb434802570fb1307e0812d10bfed94bf43
unknown <chen.baran@gmail.com>
2023-06-15 12:35:27 +0300

    15.06.2023 - 12:35 - Added option to sign in with email

--- treatnaturallystore/settings/common.py
+++ treatnaturallystore/settings/common.py
@@ -116,6 +116,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'core.backends.EmailOrUsernameModelBackend',
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
